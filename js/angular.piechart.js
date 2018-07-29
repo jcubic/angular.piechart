2cd03cef222d3be276bcf8029d5f14b2bc0b9c58
Jakub Jankiewicz <jcubic@onet.pl>
2018-07-29 17:56:48 +0200

    create arc using path for IE and Edge

--- js/angular.piechart.js
+++ js/angular.piechart.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2016 Jakub Jankiewicz <http://jcubic.pl/me>
  * Released under the MIT license
  *
- * Date: Tue, 23 Jan 2018 17:38:11 +0000
+ * Date: Sun, 29 Jul 2018 15:56:25 +0000
  */
 /* global exports module define require */
 (function (global, factory) {
@@ -21,11 +21,36 @@
     'use strict';
     var module = angular.module('piechart', []);
     var template;
-    if (/Edge\/\d.|MSIE/i.test(navigator.userAgent)) {
+    // arc based on https://stackoverflow.com/a/18473154/387194
+    function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = angleInDegrees * Math.PI / 180.0;
+        return {
+            x: centerX + (radius * Math.cos(angleInRadians)),
+            y: centerY + (radius * Math.sin(angleInRadians))
+        };
+    }
+
+    function describeArc(x, y, radius, startAngle, endAngle) {
+        var start = polarToCartesian(x, y, radius, endAngle);
+        var end = polarToCartesian(x, y, radius, startAngle);
+        var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+        var d = [
+            "M", start.x, start.y,
+            "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y,
+            "L", x, y,
+            "L", start.x, start.y,
+            "Z"
+        ].join(" ");
+
+        return d;
+    }
+
+    if (true || /Edge\/\d.|MSIE/i.test(navigator.userAgent)) {
         template = '<svg viewBox="0 0 32 32" ng-style="{\'background-color\': vm.color}">'+
-            '<circle ng-if="part.value" id="circle-{{$index}}" r="16" cx="16" cy="16" '+
-            '        ng-repeat="part in vm.parts" ng-attr-transform="rotate({{part.offset}} 16 16)" '+
-            '        stroke="{{part.color}}" ng-attr-stroke-dasharray="{{part.value}} 100" />'+
+            '<path id="circle-{{$index}}" ng-attr-d="{{vm.arc(16, 16, 16, 0, part.angle)}}"'+
+            '        ng-repeat="part in vm.parts" ng-attr-transform="rotate({{part.offset}})"'+
+            '        ng-attr-foo="{{part.start + \' \' + part.end}}"' +
+            '        fill="{{part.color}}"/>' +
             '<g ng-transclude></g>'+
             '</svg>';
     } else {
@@ -51,25 +76,27 @@
     function controller($scope) {
         var maxValue = 100;
         function sumValues(items, offset) {
-            return Math.floor(items.reduce(function(count, part) {
-                count += part.value;
-                if (offset && part.value > 0 && part.value < maxValue) {
-                    count -= offset;
-                }
-                return count;
-            }, 0));
+            return items.reduce(function(count, part) {
+                return count + part.value;
+            }, 0);
         }
         var self = this;
         function update() {
+            function toDeg(num) {
+                return 360 * num / 100;
+            }
             if (self.ngModel.$modelValue) {
                 self.parts = self.ngModel.$modelValue.data.slice();
                 self.color = self.ngModel.$modelValue.color;
-                for (var i=0; i<self.parts.length; ++i) {
-                    self.parts[i].offset = Math.round((sumValues(self.parts.slice(0, i)) * 360) / 100);
+                var sum = sumValues(self.parts);
+                for (var i = 0; i < self.parts.length; ++i) {
+                    self.parts[i].angle = toDeg(self.parts[i].value);
+                    self.parts[i].offset = Math.round(toDeg(sumValues(self.parts.slice(0, i))));
                 }
             }
         }
         $scope.$watch(function() { return self.ngModel.$modelValue; }, update, true);
+        self.arc = describeArc;
     }
     controller.$inject = ['$scope'];
     return module;
